133d182446e0a73dea3c1a2163245f46492c2229
ThaoNP <npthao1312@gmail.com>
2021-08-26 15:32:45 +0700

    :star: Display all posts from firestore

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,27 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import { getAllPost, getSinglePost } from '../utils/db';
 import Link from 'next/link'
 import Date from '../components/date'
 import { GetStaticProps } from 'next'
+import React from 'react';
+import { useRouter } from 'next/router';
 
-export default function Home({
-  allPostsData
-}: {
-  allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
-}) {
+const Home = (props) => {
+  const post = JSON.parse(props.post);
+  const router = useRouter();
+
+  const generatePostCard = (singlePost) => {
+    return (
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <ul className={utilStyles.list}>
+            {singlePost.title}
+            {singlePost.content}
+          </ul>
+        </section>
+    )
+  }
   return (
     <Layout home>
       <Head>
@@ -23,35 +31,26 @@ export default function Home({
       <section className={utilStyles.headingMd}>
         <p>
           Hello. I'm Thao. I'm learning NextJS.
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn"> Next.js tutorial</a>.)
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
+      <h2 className={utilStyles.headingLg}>Blog</h2>
+          {post.map((singlePost) => (
+            <div
+              key={singlePost.id}
+            >
+              {generatePostCard(singlePost)}
+            </div>
           ))}
-        </ul>
-      </section>
     </Layout>
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
+export async function getServerSideProps(_context) {
+  const post = await getAllPost();
+  const data = post.map((singlePost: any) => {
+    return { ...singlePost};
+  });
+  return { props: { post: JSON.stringify(data) } };
 }
+
+export default Home;
