d60079b578c95f8dd6ae261d36bb60ae1be97ee2
ThaoNP <npthao1312@gmail.com>
2021-08-26 14:43:32 +0700

    :star: install dependencies

--- ultis/db.ts
+++ ultis/db.ts
@@ -1,7 +1,7 @@
 import firebase from '../lib/firebase';
 
 export const getAllPost = async () => {
-  const snapshot = await firebase.firestore().collection('post').get();
+  const snapshot = await firebase.firestore().collection('posts').get();
   const post = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   return post;
 };
