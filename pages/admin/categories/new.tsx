44b9d328d73bae20fc20a195f6632b4670cd73ba
ThaoNP <npthao1312@gmail.com>
2021-09-01 10:46:23 +0700

    ➖ remove yarn.lock

---
+++ pages/admin/categories/new.tsx
@@ -0,0 +1,73 @@
+import { Field, FieldArray, Form, Formik, getIn } from 'formik';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import * as yup from 'yup';
+import { addCategoryApi } from '../../../utils/service';
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Layout from '../../../components/layout'
+import Head from 'next/head'
+
+const NewCategory = () => {
+  const router = useRouter();
+
+  const initialValues = {
+    content: '',
+  };
+
+  const validationSchema = yup.object().shape({
+    content: yup.string().required('Required'),
+  });
+
+  const submitHandler = async (values, actions) => {
+    try {
+      values = {
+        ...values,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      await addCategoryApi(values);
+      router.push('/');
+    } catch (error) {
+      console.log('error', error);
+    } finally {
+      actions.setSubmitting(false);
+    }
+  };
+
+  return (
+    <Layout>
+      <Head>
+        <title>Create Category</title>
+      </Head>
+      <Container>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={submitHandler}
+          validationSchema={validationSchema}
+        >
+          {(props) => (
+            <Form>
+              <Field name="content">
+                {({ field, form }) => (
+                  <div className="form-group mb-3">
+                    <label htmlFor="content">Category Content</label>
+                    <textarea className="form-control" id="content" {...field} rows="5"/>
+                    {form.errors.description}
+                  </div>
+                )}
+              </Field>
+              <div className="d-flex justify-content-center">
+                <Button type="submit">
+                  Submit Category
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </Container>
+    </Layout>
+  );
+};
+
+export default NewCategory;
